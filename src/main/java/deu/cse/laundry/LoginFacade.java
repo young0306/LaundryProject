4444f4e55eae37d4ab7782c1e2433cfa20a5ef44
kimeunbinbev <94042060+kimeunbinbev@users.noreply.github.com>
2024-05-10 23:36:53 +0900

    넷빈즈 수정중

--- src/main/java/deu/cse/laundry/LoginFacade.java
+++ src/main/java/deu/cse/laundry/LoginFacade.java
@@ -11,17 +11,22 @@ import javax.swing.JOptionPane;
  *
  * @author hesed
  */
+// 퍼사드 패턴을 적용한 클래스
 public class LoginFacade {
+    // realLogin, 실제 로그인 기능을 수행하는 객체
     private Login realLogin;
+    
+     // LoginFacade 생성자에서 realLogin 객체를 초기화
     public LoginFacade() {
         this.realLogin = new RealLogin();
     }
-
+    
+    // login 함수, ID와 비밀번호를 받아 realLogin 객체의 login 함수를 호출, 결과 반환
     public boolean login(String ID, String Password) {
         return realLogin.login(ID, Password);
     }
-    
-    public void logout(){
+    // exitstart 함수, 로그아웃 메시지 출력 후 종료
+    public void exitstart(){
         JOptionPane.showMessageDialog(null, "시스템을 종료합니다.", "안내", JOptionPane.INFORMATION_MESSAGE);
         System.exit(0);
     }
