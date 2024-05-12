f17b20c67cda060600b916fe0a96227df2fe26a5
young0306 <150253108+young0306@users.noreply.github.com>
2024-05-12 18:03:10 +0900

    test

---
+++ src/main/java/deu/cse/laundry/WashingMachine.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deu.cse.laundry;
+
+/**
+ *
+ * @author pc22-1008
+ */
+public class WashingMachine {
+    
+}
